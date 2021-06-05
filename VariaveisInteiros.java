5b05955322072dd1da34547105b59ace87e94d0b
Heyla36 <gumanum123@gmail.com>
2021-06-05 15:10:17 -0300

    Classes

---
+++ VariaveisInteiros.java
@@ -0,0 +1,19 @@
+package variaveis.aula11;
+
+public class VariaveisInteiros {
+
+	public static void main(String[] args) {
+		
+		byte idade01 = 01;
+		short idade02 = 20;
+		int idade03 = 45;
+		long idade04 = 300;
+		
+		System.out.println("Valor variável idade = " + idade01);
+		System.out.println("Valor variável idade = " + idade02);
+		System.out.println("Valor variável idade = " + idade03);
+		System.out.println("Valor variável idade = " + idade04);
+
+	}
+
+}
